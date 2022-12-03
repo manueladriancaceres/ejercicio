616386861c82986f50022300a6baddcbf5dc548d
manueladriancaceres <adriancaceres980@gmail.com>
2022-12-03 10:59:21 -0300

    Update App.java

--- 2. java/0-holamundo/untitled/src/com/meli/bootcamp/ejericio/App.java
+++ 2. java/0-holamundo/untitled/src/com/meli/bootcamp/ejericio/App.java
@@ -3,7 +3,7 @@ package com.meli.bootcamp.ejericio;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo....");
     }
 
 }
